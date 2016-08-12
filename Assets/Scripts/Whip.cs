2c26376816fb6c34cd0585415fe884d39abe38a5
Matt Robinson <mrobinson7627@gmail.com>
2016-08-11 22:32:19 -0400

    First commit

---
+++ Assets/Scripts/Whip.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System;
+
+[RequireComponent(typeof(BoxCollider2D))]
+[RequireComponent(typeof(MeshRenderer))]
+public class Whip : MonoBehaviour {
+
+    BoxCollider2D boxCollider;
+    MeshRenderer meshRenderer;
+
+    public enum Direction {
+        Left, Right
+    }
+
+    public Direction WhipDirection {
+        set {
+            var x = transform.localPosition.x;
+            var y = transform.localPosition.y;
+            switch (value) {
+                case Direction.Left:
+                    transform.localPosition = new Vector3(-Math.Abs(x), y, 0);
+                    break;
+                case Direction.Right:
+                    transform.localPosition = new Vector3(Math.Abs(x), y, 0);
+                    break;
+            }
+        }
+    }
+
+    // Use this for initialization
+    void Start() {
+        boxCollider = GetComponent<BoxCollider2D>();
+        boxCollider.enabled = false;
+
+        meshRenderer = GetComponent<MeshRenderer>();
+        meshRenderer.enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if (Input.GetKeyDown(KeyCode.Z)) {
+            boxCollider.enabled = !boxCollider.enabled;
+            meshRenderer.enabled = !meshRenderer.enabled;
+        }
+
+        Debug.Log(transform.position);
+    }
+}
