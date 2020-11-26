c434aea0f6ad9e403d5604b1ab28a625347f347f
🌺 Megan Robinson 🌺 <mrobinson7627@gmail.com>
2020-11-25 19:09:55 -0500

    Rename a namespace

--- Assets/Scripts/SceneLoader/Scene_1_2.cs
+++ Assets/Scripts/SceneLoader/Scene_1_2.cs
@@ -1,9 +1,11 @@
 using System.Collections.Generic;
-using Scene;
+using SceneLoader;
 
+namespace SceneLoader {
 class Scene_1_2 : IScene {
     public const string Name = "1-2";
     public string name => Name;
 
     public List<IScene> GetNeighbors() => new List<IScene>() { new Scene_1_1(), new Scene_1_3() };
+}
 }
