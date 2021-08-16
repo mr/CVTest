17194f3cc0e70b28a3c5eb4052cd703d59eedc6b
Megan Robinson <mrobinson7627@gmail.com>
2021-08-15 23:13:03 -0400

    Update camera follow to flip between horizontal and vertical
    This isn't a very good solution but it makes it playable at the very
    least.

--- Assets/Scripts/CVCameraFollow.cs
+++ Assets/Scripts/CVCameraFollow.cs
@@ -1,7 +1,11 @@
 ﻿using UnityEngine;
+using System.Linq;
 
 [RequireComponent(typeof(Camera))]
 public class CVCameraFollow : MonoBehaviour {
+
+    public static string MODE_HORIZONTAL_TAG = "Horizontal";
+    public static string MODE_VERITCAL_TAG = "Vertical";
 
     public Controller2D target;
 
@@ -16,6 +20,8 @@ public class CVCameraFollow : MonoBehaviour {
     }
 
     public FollowMode followMode = FollowMode.Horizontal;
+
+    private bool movedBefore = false;
 
     void Start() {
         camera = GetComponent<Camera>();
@@ -39,13 +45,37 @@ public class CVCameraFollow : MonoBehaviour {
         var targetDistanceSign = Mathf.Sign(targetDistance);
         var bounds = OrthographicBounds(camera);
         var origin = targetDistanceSign == -1 ? bounds.min : bounds.max;
-        var hit = Physics2D.Raycast(origin, direction * targetDistanceSign, Mathf.Infinity, collisionMask);
+        var hits =
+            from hit in Physics2D.RaycastAll(
+                origin, direction * targetDistanceSign, Mathf.Infinity, collisionMask)
+            where hit.collider.gameObject.CompareTag(
+                followMode == FollowMode.Horizontal
+                    ? MODE_HORIZONTAL_TAG
+                    : MODE_VERITCAL_TAG)
+            select hit;
+        var firstHit = hits.FirstOrDefault();
 
+        bool hitBarrier = false;
         var velocity = direction * targetDistance;
-        if (hit) {
-            var hitDistance = hit.distance * velocity.normalized;
+        if (firstHit) {
+            var hitDistance = firstHit.distance * velocity.normalized;
             if (hitDistance.magnitude < velocity.magnitude) {
                 velocity = hitDistance;
+                hitBarrier = true;
+            }
+        }
+
+        if (hitBarrier) {
+            if (movedBefore) {
+                if (followMode == FollowMode.Horizontal) {
+                    followMode = FollowMode.Vertical;
+                } else if (followMode == FollowMode.Vertical) {
+                    followMode = FollowMode.Horizontal;
+                }
+                movedBefore = false;
+            }
+            if (velocity.magnitude != 0) {
+                movedBefore = true;
             }
         }
 
